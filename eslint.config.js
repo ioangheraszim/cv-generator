33090569236542116ae63952b44eb0f6ef3856ed
ioangheraszim <gijohny17@gmail.com>
2025-06-28 16:10:07 +0100

    Initialized Vite React project with ESLint and Prettier setup - Created project using vite + react template - Installed and configured ESLint - Integrated Prettier with ESLint for code formatting - Added .prettierrc and .eslintrc config files

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      prettier: pluginPrettier,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error',
+      'react/prop-types': 'off',
+    },
+  },
+]);
